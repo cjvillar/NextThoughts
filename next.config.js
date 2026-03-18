d9f8e03d0dd2f605d9c686307436c1918edef03b
Chris Villarreal <cjvillar@users.noreply.github.com>
2026-03-18 16:56:52 -0700

    fixed outdated packages

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
+      serverExternalPackages: ["mongoose"],
     images: {
-      domains: ['lh3.googleusercontent.com'],
+      remotePatterns: [{protocol: 'https', hostname: 'lh3.googleusercontent.com'} ],
     },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
-    }
   }
   
   module.exports = nextConfig

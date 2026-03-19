9a4f8ad28ddb09e7ab0bd9fa8dc331b761c2f2df
Chris Villarreal <cjvillar@users.noreply.github.com>
2026-03-18 20:55:19 -0700

    updated routes

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -6,7 +6,7 @@ export const GET = async (request, { params }) => {
     try {
         await connectToDB()
 
-        const prompt = await Prompt.findById(params.id).populate("creator")
+        const prompt = await Prompt.findById(params.id).populate("creator", "-email")
         if (!prompt) return new Response("Thought Not Found", { status: 404 });
 
         return new Response(JSON.stringify(prompt), { status: 200 })

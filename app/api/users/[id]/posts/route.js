8d683384d9eb0f6d916e4cb032416ee1a6b415b2
Chris Villarreal <cjvillar@users.noreply.github.com>
2026-03-18 21:50:51 -0700

    updated post routes

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,19 +1,17 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
+import { Types } from "mongoose";
 
 export const GET = async (request, { params }) => {
-    try{
-        await connectToDB();
-        
-        const prompts = await Prompt.find({creator: params.id}).populate('creator');
+  try {
+    await connectToDB();
 
-        return new Response(JSON.stringify(prompts),{
-            status: 200
-        })
-    }catch(error){
-        return new Response("Failed to fetch all thoughts",{
-            status: 500
-        })
+    const prompts = await Prompt.find({ 
+      creator: new Types.ObjectId(params.id)
+    }).populate('creator', '-email');
 
-    }
-} 
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch all thoughts", { status: 500 });
+  }
+};
